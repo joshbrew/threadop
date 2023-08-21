c969ee2b8fa7d6f8bc90d29fb6481e1ad1ed55b3
Joshua Brewster (Bubba Skrimp) <brewster.joshua1@gmail.com>
2023-08-21 02:42:39 -0700

    add dist

---
+++ dist/threadop.d.ts
@@ -0,0 +1,30 @@
+export type ModuleImport = {
+    [modulePath: string]: string | boolean | {
+        [importName: string]: string | boolean;
+    };
+};
+export type ImportsInput = string | string[] | ModuleImport;
+export type WorkerHelper = {
+    run: (message: any, transfer?: Transferable[]) => Promise<any>;
+    terminate: () => void;
+    addPort: (port: Worker) => void;
+    addCallback: (callback?: (data: any) => void, oneOff?: boolean) => number;
+    removeCallback: (cb: number) => void;
+    worker: Worker;
+    callbacks: {
+        [key: number]: (data: any, cb?: number) => void;
+    };
+};
+export declare function threadOp(callback?: (data: any) => any, options?: {
+    imports?: ImportsInput;
+    message: any;
+    transfer?: Transferable[];
+    port?: Worker;
+    blocking?: boolean;
+}): Promise<any>;
+export declare function threadOp(callback?: (data: any) => any, options?: {
+    imports?: ImportsInput;
+    transfer?: Transferable[];
+    port?: Worker;
+    blocking?: boolean;
+}): Promise<WorkerHelper>;
